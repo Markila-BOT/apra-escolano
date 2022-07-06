94095484f5b203d13609aa4371d63a9f18d8af91
Mark <mark.escolano@wealth-park.com>
2022-07-06 16:40:53 +0800

    add loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Gallery } from "../interfaces";
 import PhotoGallery from "../components/PhotoGallery";
 import { API_URL } from "../constants";
 import { debounce } from "ts-debounce";
+import Loader from "../components/Loader";
 
 type IndexTypes = {
   gallery: Gallery[];
@@ -11,13 +12,16 @@ type IndexTypes = {
 const Index: FC<IndexTypes> = () => {
   const [gallery, setGallery] = useState<Gallery[]>([]);
   const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const requestURL = query ? `${API_URL}?title_like=${query}` : API_URL;
 
   useEffect(() => {
     const fetchPhotos = async () => {
+      setIsLoading(true);
       const res = await fetch(requestURL);
       const gallery: Gallery[] = await res.json();
       setGallery(gallery);
+      setIsLoading(false);
     };
 
     const debouncedFetchPhotos = debounce(fetchPhotos, 1500);
@@ -26,8 +30,15 @@ const Index: FC<IndexTypes> = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 overflow-x-auto bg-slate-100 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-      <SearchBar query={query} setQuery={setQuery} />
-      <PhotoGallery gallery={gallery} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <SearchBar query={query} setQuery={setQuery} />
+          <PhotoGallery gallery={gallery} />
+        </>
+      )}
     </div>
   );
 };
